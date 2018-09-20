c49933b6548a0cef11a37d4772819ec73f33454b
pdn1905 <phan.duong.ngoc@framgia.com>
2018-09-20 15:41:04 +0700

    Initial Commit

---
+++ XibLocalize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XibLocalize
+//
+//  Created by phan.duong.ngoc on 9/20/18.
+//  Copyright © 2018 Framgia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
